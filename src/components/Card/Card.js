4e2e41f48c6fc737989d8f0c05f506dec78ad346
Małgorzata Jańczak <m.janczak@int.pl>
2019-10-21 15:01:12 +0200

    add cards

---
+++ src/components/Card/Card.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Card.scss';
+
+class Card extends React.Component {
+  static propTypes = {
+    title: PropTypes.string,
+  }
+
+  static defaultProps = {
+    title: 'To do',
+  }
+
+  render() {
+    return (
+      <section className={styles.component}>
+        <h3>{this.props.title}</h3>
+      </section>
+    )
+  }
+}
+
+export default Card;
